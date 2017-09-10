f9745139934ca2e3b48a727d90302e381df01f66
zorahima <zorahima10@gmail.com>
2017-09-10 21:55:58 +0700

    PART 2

---
+++ 06_PART2_09459.php
@@ -0,0 +1,12 @@
+<?php
+function name($fname, $lname= " ")
+{
+	echo " $fname  Ahmad $lname <br/>";
+
+}
+echo "Berikut nama mahasiswa yang mempunyai nama depan Ahmad: <br/>";
+
+echo name ("", "Latif");
+echo name ("","Fadillah");
+echo name ("","Zakaria");
+?> 
