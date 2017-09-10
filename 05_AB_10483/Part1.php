ae24ead0ee9308f827499206ff58d132f1a34441
Daniel Siddiq <daniel.sidix@gmail.com>
2017-09-10 23:45:04 +0700

    Menambahkan tugas part1

---
+++ 05_AB_10483/Part1.php
@@ -0,0 +1,25 @@
+//Nama: Daniel Siddiq
+//NIM : 16/396270/SV/10483
+<?php
+
+	echo "--------Data Mahasiswa-------"
+	
+	function mahasiswa($NIM, $Nama, $Kelas, $Prodi){
+		echo "NIM : $NIM, Nama : $Nama, Kelas : $Kelas, Prodi : $Prodi <br />";
+	}	
+			mahasiswa(01,"Robeat", "B", "Komunikasi");
+			mahasiswa(02,"Radkill", "AA", "Perikanan");
+			mahasiswa(03,"Jmbx", "C2", "Kelautan");
+			mahasiswa(04,"Alem", "C1", "Ilkomp");
+			mahasiswa(05,"Aldabx", "AB", "Pariwisata");
+			mahasiswa(06,"Napom", "BB", "Sosiologi");
+			mahasiswa(07,"Sasuke", "D", "Filsafat");
+			mahasiswa(08,"Naruto", "AD", "Pemerintahan");
+			mahasiswa(09,"Sakura", "C", "Medis");
+			mahasiswa(10,"Kakasi", "VIP", "Ninjutsu");
+			
+	arsort($Nama);
+	Print_r(mahasiswa);
+	
+	}
+?>
