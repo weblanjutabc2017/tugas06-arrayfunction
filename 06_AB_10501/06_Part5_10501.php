44104649766c83c015c9181830e0a743d2b91546
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2017-09-10 23:21:51 +0700

    Update Part 5 V2

--- 06_AB_10501/06_Part5_10501.php
+++ 06_AB_10501/06_Part5_10501.php
@@ -1,40 +1,42 @@
 <?php
-	$gan = array(1);
-	$gen = array(1);
+	$genap = array();
+	$ganjil = array();
+	$input = 10;
+	$tmp = 0;
+	$temp = 0;
+	$tmp1 = 0;
+	$temp1 = 0;
 	
-	function deret_ganjil($input){
-		$tmp1 = 0;
-		$ins1 = 0;
-		for ($i = 1;$i <= $input;$i++){
-			if ($i % 2 != 0){
-				$gan[$ins1] = $i;
-				$ins1++;
-				echo $gan[$ins1];
-			}
+	//for genap TEMP
+	for ($i = 1; $i < $input; $i++){
+		if ($i % 2 == 0){
+			$genap[$tmp] = $i;
+			$tmp++;
 		}
-		for ($i = 0;$i < $ins1;$i++){
-			$tmp1 += $gan[$i];
-		}
-		return $tmp1;
 	}
 	
-	function deret_genap($input){
-		$tmp2 = 0;
-		$ins2 = 0;
-		for ($i = 1;$i <= $ins2;$i++){
-			if ($i % 2 == 0){
-				$gen[$ins2] = $i;
-				$ins2++;
-				echo $gen[$ins2];
-			}
+	//for ganjil TEMP
+	for ($i = 1; $i < $input; $i++){
+		if ($i % 2 != 0){
+			$ganjil[$tmp1] = $i;
+			$tmp1++;
 		}
-		for ($i = 0;$i < $ins2;$i++){
-			$tmp2 += $gen[$i];
-		}
-		return $tmp2;
 	}
 	
-	echo deret_genap(20); echo "<br/>";
-	//echo deret_ganjil(20);
+	$test = count($genap);
+	$test1 = count($ganjil);
+	
+	//for genap TEMP
+	for ($i = 0; $i < $test; $i++){
+		$temp = $temp + $genap[$i];
+	}
+	
+	//for ganjil TEMP
+	for ($i = 0; $i < $test1; $i++){
+		$temp1 = $temp1 + $ganjil[$i];
+	}
+	
+	echo "Deret bilangan genap kurang dari $input = ".$temp; echo "<br/>";
+	echo "Deret bilangan ganjil kurang dari $input = ".$temp1;
 	
 ?>
