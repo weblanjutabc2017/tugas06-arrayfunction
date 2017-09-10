efc24c6607b8363267d26c87e9774c89116fc387
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2017-09-10 22:49:38 +0700

    Update Part 1 V2

--- 06_AB_10501/06_Part1_10501.php
+++ 06_AB_10501/06_Part1_10501.php
@@ -2,18 +2,21 @@
 	
 	$mhs = array
 	(
-	array("001", "Ariq", "B", "Komsi"),
-	array("002", "Kira", "B", "Komsi"),
-	array("003", "Akira", "B", "Elins"),
-	array("004", "Ann", "A", "Komsi"),
-	array("005", "Yuuki", "C", "RekMed"),
-	array("006", "Tango", "AB", "Mesin"),
-	array("007", "Malika", "C1", "Pertanian"),
-	array("008", "Bango", "A", "Pertanian"),
-	array("009", "Kedelai Hitam", "C", "Komsi"),
-	array("010", "Hifumin", "BB", "Komsi"),
+	"Ariq" => array("001", "B", "Komsi"),
+	"Kira" => array("002", "B", "Komsi"),
+	"Akira" => array("003", "B", "Elins"),
+	"An" => array("004", "A", "Komsi"),
+	"Yuuki" => array("005", "C", "RekMed"),
+	"Tango" => array("006", "AB", "Mesin"),
+	"Malika" => array("007", "C1", "Pertanian"),
+	"Bango" => array("008", "A", "Pertanian"),
+	"Kedelai Hitam" => array("009", "C", "Komsi"),
+	"TEST" => array("010", "BB", "Komsi"),
 	);
 	
+	ksort($mhs);
+	
+	/*
 	for ($i = 0;$i < 10;$i++){
 		echo "<p><b>Mahasiswa $i+1</p></b>";
 		echo "<ul>";
@@ -21,6 +24,14 @@
 			echo "<li>".$mhs[$i][$j]."</li>";
 		}
 		echo "</ul>";
+	}
+	*/
+	foreach ($mhs as $i => $i_value){
+		echo $i."<br/>";
+		for ($z = 0;$z < 3;$z++){
+			echo $i_value[$z]."<br/>";
+		}
+		echo "<br/>";
 	}
 	
 ?>
