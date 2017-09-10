379f1606adc0235078a8edaef4d11d0dce84af27
Daniel Siddiq <daniel.sidix@gmail.com>
2017-09-10 23:57:12 +0700

    Menambahkan Tugas 6 Part2

---
+++ 06_AB_10483/Part2.php
@@ -0,0 +1,12 @@
+<?php
+
+	function name($fname= "James", $firstname){
+	
+	$name = $fname." ".$firstnamne;
+	
+	return $name;
+}
+
+echo "Berikut ini adalah nama mahasiswa yang memiliki nama depan James : <br />".name("James","George")."<br />".name("james","Siti")."<br />".name("James","Fardan")."<br />";
+
+?>
